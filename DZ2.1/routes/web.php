a9b1e66bdded291fb99ef48de04f15ab9626d03b
AntonioPavkovic <antoniopavkovic999@gmail.com>
2023-05-01 20:25:27 +0200

    finished DZ2.1

--- DZ2.1/routes/web.php
+++ DZ2.1/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegionController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\JobController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +18,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('regions', RegionController::class);
+Route::resource('countries', CountryController::class);
+Route::resource('locations', LocationController::class);
+Route::resource('departments', DepartmentController::class);
+Route::resource('employees', EmployeeController::class);
+Route::resource('jobs', JobController::class);
 
 Route::get('/', function () {
     return view('welcome');
